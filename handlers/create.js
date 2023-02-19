e37946e33108ecf0ca05e31f9d8312553f5fc3da
Eric Syu <xeriok02390@gmail.com>
2023-02-20 00:54:07 +0800

    Add create function AND modify settings

---
+++ handlers/create.js
@@ -0,0 +1,21 @@
+'use strict';
+const AWS = require('aws-sdk');
+
+module.exports.create = async (event) => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+  const body = JSON.parse(Buffer.from(event.body, 'base64').toString());
+
+  const putParams = {
+    TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
+    Item: {
+      primary_key: body.name,
+      url: body.url,
+    },
+  };
+  await dynamoDb.put(putParams).promise();
+ 
+  return {
+    statusCode: 200,
+    body: JSON.stringify(putParams)
+  };
+};
--- handlers/index.js
+++ handlers/index.js
@@ -1,25 +1,30 @@
 'use strict';
 const AWS = require('aws-sdk');
-const docClient = new AWS.DynamoDB.DocumentClient();
-
-const params = {
-  TableName : process.env.TABLE_NAME
-}
-
-async function Items(){
-  try {
-    const data = await docClient.scan(params).promise()
-    return data
-  } catch (err) {
-    return err
-  }
-}
 
 module.exports.index = async (event) => {
-  try {
-    const data = await Items()
-    return { body: JSON.stringify(data) }
-  } catch (err) {
-    return { error: err }
+  const scanParams = {
+    TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
+  };
+ 
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+  const result = await dynamoDb.scan(scanParams).promise();
+ 
+  if (result.Count === 0) {
+    return {
+      statusCode: 404,
+    };
   }
+ 
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      total: result.Count,
+      items: await result.Items.map((store) => {
+        return {
+	  name: store.primary_key,
+          url: store.url,
+        };
+      }),
+    }),
+  };
 };
